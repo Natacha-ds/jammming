d5def55ceebd4cfcc151171721397dabf4ec425d
Natacha <natacha.dehass@gmail.com>
2020-05-18 18:48:40 +0200

    Updates

---
+++ precache-manifest.0b5195ff807af1c7630676212a520b50.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1525daa8cf627d2de1cd9e3a846db12",
+    "url": "/jammming/index.html"
+  },
+  {
+    "revision": "606c5e6565736e3d15c3",
+    "url": "/jammming/static/css/main.517d711e.chunk.css"
+  },
+  {
+    "revision": "eeee13a0ed6a817aa796",
+    "url": "/jammming/static/js/2.6d23d5a0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/jammming/static/js/2.6d23d5a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "606c5e6565736e3d15c3",
+    "url": "/jammming/static/js/main.c17bd6f3.chunk.js"
+  },
+  {
+    "revision": "e09f1e32bf8a78998d52",
+    "url": "/jammming/static/js/runtime-main.542e1b1d.js"
+  },
+  {
+    "revision": "37be7357a5f8583073df7e0aec787eaa",
+    "url": "/jammming/static/media/background_photo_desktop.37be7357.jpg"
+  }
+]);
